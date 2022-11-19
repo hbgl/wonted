87df54f6899678020d3d105121f5790e9c93bb1e
hbgl <gohbgl@gmail.com>
2022-11-19 06:21:27 +0100

    Refactor.

--- src/path.ts
+++ src/path.ts
@@ -2,12 +2,12 @@ import os from 'node:os';
 import nodePath from 'node:path';
 
 export function isSubdir(parent: string, maybeChild: string) {
-    parent = normalizeForCmp(parent);
-    maybeChild = normalizeForCmp(maybeChild);
+    parent = normalizeDirForCmp(parent);
+    maybeChild = normalizeDirForCmp(maybeChild);
     return parent.length < maybeChild.length && maybeChild.startsWith(parent);
 }
 
-function normalizeForCmp(path: string) {
+function normalizeDirForCmp(path: string) {
     if (os.platform() === 'win32') {
         // Strip long path prefix.
         if (path.startsWith('\\\\?\\')) {
